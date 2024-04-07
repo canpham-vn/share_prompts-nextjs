b92fe5d8f3ae6d92e05d60cbe1567cc8baf7d046
canpham-vn <canpham0610@gmail.com>
2024-04-07 22:31:01 +0700

    handle login google auth

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -10,33 +10,35 @@ const handler = NextAuth({
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  async session({ session }) {
-    const sessionUser = await User.findOne({ email: session.user.email });
+  callbacks: {
+    async session({ session }) {
+      const sessionUser = await User.findOne({ email: session.user.email });
 
-    session.user.id = sessionUser._id.toString();
+      session.user.id = sessionUser._id.toString();
 
-    return session;
-  },
-  async signIn({ profile }) {
-    try {
-      await connectToDB();
+      return session;
+    },
+    async signIn({ profile }) {
+      try {
+        await connectToDB();
 
-      // check if the user is already existing
-      const userExists = await User.findOne({ email: profile.email });
+        // check if the user is already existing
+        const userExists = await User.findOne({ email: profile.email });
 
-      // if not, create a new user
-      if (!userExists) {
-        await User.create({
-          email: profile.email,
-          username: profile.name.replace(" ", "").toLowerCase(),
-          image: profile.picture,
-        });
+        // if not, create a new user
+        if (!userExists) {
+          await User.create({
+            email: profile.email,
+            username: profile.name.replace(" ", "").toLowerCase(),
+            image: profile.picture,
+          });
+        }
+
+        return true;
+      } catch (error) {
+        console.log(error);
       }
-
-      return true;
-    } catch (error) {
-      console.log(error);
-    }
+    },
   },
 });
 
